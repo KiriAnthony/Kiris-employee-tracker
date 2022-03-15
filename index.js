f8eb7bef52a920f16cae2070e563d358edaff872
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-14 21:32:30 -0700

    applying user input to switch case

--- index.js
+++ index.js
@@ -8,13 +8,46 @@ const cTable = require('console.table');
 const startingOptions = ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'];
 
 // add functions to call depending on user response from inquirer
+const viewDepartment = () => {
+    // function to display table with formatted names and ids from department
+    console.log("viewing department");
+};
 
+const viewRoles = () => {
+    // function to display table with job title, role id, department role belongs to, and salary
+    console.log("viewing role");
+};
+
+const viewEmployees = () => {
+    // function to display table with employee ids, first/last names, job title, department, salary, and managers
+    console.log("viewing employee");
+};
+
+const addDepartment = () => {
+    // function to prompt for name of department then department is added to database
+    console.log("adding department");
+};
+
+const addRole = () => {
+    // function to prompt for name, salary, and department then role added to database
+    console.log("adding role");
+};
+
+const addEmployee = () => {
+    // function to prompt for employee first/last name, role, and manager then employee added to database
+    console.log("adding employee");
+};
+
+const updateEmployeeRole = () => {
+    // function to select an employee to update and prompt for new role, info updated in database
+    console.log("updating employee role");
+};
 
 // run inquirer to prompt user for first selection
 const firstPromtUser = () => {
     return inquirer.prompt([
         {
-            type: 'checkbox',
+            type: 'list',
             name: 'firstSelection',
             message: "What would you like to do?",
             choices: startingOptions
@@ -22,4 +55,37 @@ const firstPromtUser = () => {
     ]);
 };
 
-firstPromtUser().then(answer => console.log(answer));
+// calling function at beginning of application
+firstPromtUser().then((answer) => {
+    // deconstructing the answer to show the firstSelection from user
+    const {firstSelection} = answer;
+    console.log(firstSelection);
+    // using a switch case to call the correct function for next steps
+    switch(firstSelection) {
+        case startingOptions[0]: viewDepartment();
+        break;
+
+        case startingOptions[1]: viewRoles();
+        break;
+
+        case startingOptions[2]: viewEmployees();
+        break;
+
+        case startingOptions[3]: addDepartment();
+        break;
+
+        case startingOptions[4]: addRole();
+        break;
+
+        case startingOptions[5]: addEmployee();
+        break;
+
+        case startingOptions[6]: updateEmployeeRole();
+        break;
+
+        // if selection does not include one of the 6 preselected outcomes, run default
+        default: console.log("Please select an option to continue");
+        firstPromtUser();
+        break;
+    }
+});
