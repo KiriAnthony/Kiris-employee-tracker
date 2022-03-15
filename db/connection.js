61a058bc63c7e0feaab09d1b3112c154dd31c2fe
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-14 21:38:54 -0700

    adding mysql2 connection

---
+++ db/connection.js
@@ -0,0 +1,14 @@
+// connect to mysql database
+const mysql = require('mysql2');
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Password-1',
+        database: 'employee_tracker'
+    },
+    console.log('Connected to the employee_tracker database.')
+);
+
+module.exports = db;
--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const inquirer = require('inquirer');
 // import console.table for use to display tables
 const cTable = require('console.table');
 
+// import db from connection.js to allow for querys
+const db = require('./db/connection');
+
 // creates an array full of options for users first selection
 const startingOptions = ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'];
 
