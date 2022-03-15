53a28049da8d383eb0063a18ccc088b81c207f1b
Kiri Anthony <Kiriko.anthony@gmail.com>
2022-03-14 20:44:33 -0700

    adding inquire for first prompt

---
+++ index.js
@@ -0,0 +1,22 @@
+// import inquirer (after npm init -y and npm install inquirer)
+const inquirer = require('inquirer');
+
+// creates an array full of options for users first selection
+const startingOptions = ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role'];
+
+// add functions to call depending on user response from inquirer
+
+
+// run inquirer to prompt user for first selection
+const firstPromtUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'checkbox',
+            name: 'firstSelection',
+            message: "What would you like to do?",
+            choices: startingOptions
+        }
+    ]);
+};
+
+firstPromtUser().then(answer => console.log(answer));
